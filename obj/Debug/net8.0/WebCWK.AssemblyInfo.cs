3ed32e22ffc7a798b5e337c9aec8f59210ee36bd
AkinOlu21 <kenneth.akinola@outlook.com>
2024-02-27 12:40:52 +0000

    Version 3

--- obj/Debug/net8.0/WebCWK.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebCWK.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCWK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e52591a74c5dfb26a2ef740949415625adfd540c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a466b8e0cf5a96c3061ab60d19d683aee07d4b39")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCWK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCWK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
