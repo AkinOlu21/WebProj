7d9e6db8047ce47f5bf3be24162e25d628936dac
AkinOlu21 <kenneth.akinola@outlook.com>
2024-02-27 19:48:46 +0000

    version 4 - testing email verification

--- obj/Debug/net8.0/WebCWK.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebCWK.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCWK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4642ec24f55a122f4c1b8364e8d128669bef3aea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e03769941a0e47c0448d26b0746b081e72b3e911")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCWK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCWK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
