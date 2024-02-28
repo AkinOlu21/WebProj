3a86acb1740dbafda8e6f78582d111df9266f5ec
AkinOlu21 <kenneth.akinola@outlook.com>
2024-02-28 14:10:13 +0000

    Version 5

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using WebCWK.Controllers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebCWK.Models;
+using IdentityPractice.Controllers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +22,27 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>()
 
 builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
 builder.Services.AddScoped<EmailService>();
+
+ builder.Services.AddScoped<RolesController>();
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                    };
+                    });
 
 
 var app = builder.Build();
--- obj/Debug/net8.0/WebCWK.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebCWK.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCWK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e03769941a0e47c0448d26b0746b081e72b3e911")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e3c1e1f913845345d10ae2d282747c39bdd5bc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCWK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCWK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
